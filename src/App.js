3b43deed61a22b216af1e8e05fde4f4221103475
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-15 20:45:31 +0900

    평화로운 쇼핑몰 레이아웃 디자인시간

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './logo.svg';
+
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="p-5 mb-4 bg-light rounded-3 background">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">20% Season Off</h1>
+          <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+          <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        </div>
+      </div>
+
+      <div className="contaier">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+          < img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>            
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
