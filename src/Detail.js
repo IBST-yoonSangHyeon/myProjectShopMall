4d5416562e6e412dd88ca76c9f8e9b4be2859e39
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-18 21:48:52 +0900

    Lifecycle Hook (옛날사람) useEffect (요즘사람)
    1. useEffect로 라이프 싸이클 관리가능 (첫 화면에서 컴포넌트가 마운트되었을때, 컴포넌트 언마운트 되기에 )
    2. useEffect는 여러개 선언가능하지만 위에서 아래로 실행됨
    3. useEffect에서 return 콜백함수는 언마운트될때 실행됨

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components'; // css를 미리 입혀놓은 컴포넌트, className 작명 필요없음
 import './Detail.scss';
@@ -17,6 +17,19 @@ let 제목 = styled.h4`
 
 function Detail ( props ) {
 
+    // 라이프 싸이클 (컴포넌트 mount되었을때 , update될때 특정코드 () => {} 실행)
+    useEffect(() => {
+      // Detail페이지 방문후 alert창이 2초후에 사라지게 해주세요. (마운트될때)
+      setTimeout(() => { document.getElementsByClassName('my-alert2')[0].style.display = "none"; }, 2000);
+      // useEffect 훅2 컴포넌트가 사라질때 코드를 실행시킬수 있다. (언마운트될때)
+      return () => {};
+    });
+
+    // 여러개 선언 가능 단, 위에서 아래로 useEffect실행됨
+    useEffect(() => {
+
+    });
+
     let { id } = useParams(); // { 사용자가 입력한 URL 파라미터들 }, 스트럭처링 문법으로 처리
     let history = useHistory(); // 모든 방문기록이 담긴 object
     let 찾은상품 = props.shoes.find(x => x.id == id);
@@ -26,7 +39,7 @@ function Detail ( props ) {
         <박스>
           <제목 className="red" >Detail</제목>
         </박스>
-        <div className="my-alert2">
+        <div className={`my-alert2`}>
           <p>재고가 얼마 남지 않았습니다.</p>
         </div>
         <div className="row">
