9c68bd02f7a5561db97279ff4db73840f7f68bae
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-18 22:11:56 +0900

    useEffect 숙제 풀이 & 나머지 기능
    훅 라이프싸이클 조건문
    [] : 처음에 실행하고, 재렌더링될때 실행하지 말아주세요.
    [alert] : alert state가 변경될때만 실행해주세요.
    [alert, input] : alert 또는 input state가 변경될때 실행해주세요.
    
    setTimeout은 모르는 에러가 발생할수 있으니 clearTimeout을 return 해주세요.
    return은 useEffect에서는 해당 컴포넌트가 사라지기 전에 실행됩니다.

--- src/Detail.js
+++ src/Detail.js
@@ -16,19 +16,21 @@ let 제목 = styled.h4`
 
 
 function Detail ( props ) {
+    let [alert, alert변경] = useState(true);
+    let [inputData, inputData변경] = useState("");
 
     // 라이프 싸이클 (컴포넌트 mount되었을때 , update될때 특정코드 () => {} 실행)
     useEffect(() => {
-      // Detail페이지 방문후 alert창이 2초후에 사라지게 해주세요. (마운트될때)
-      setTimeout(() => { document.getElementsByClassName('my-alert2')[0].style.display = "none"; }, 2000);
-      // useEffect 훅2 컴포넌트가 사라질때 코드를 실행시킬수 있다. (언마운트될때)
-      return () => {};
-    });
-
-    // 여러개 선언 가능 단, 위에서 아래로 useEffect실행됨
-    useEffect(() => {
-
-    });
+      // 컴포넌트 등장시 실행됨
+      let 타이머 = setTimeout(() => { alert변경(false) }, 2000);
+      // 컴포넌트 업데이트시 실행됨
+      return () => { clearTimeout(타이머) }; // Detail컴포넌트가 사라질때 , setTimout쓸때(모르는 에러 발생할수 있음) 명심해주세요. TIP
+    }, []); // [] : useEffect 훅 4 특정 state가 변결될때만 실행해주세요~ 
+    /*
+      [] : 한번만 실행해주시요. 나중에는 실행할 일이 없습니다. (재렌더링(업데이트될때는) 하지말아주세요.)
+      [alert] : alert state가 변경될때만 실행해 주세요. 그렇지 않으면 실행하지 말아주세요.
+      [alert, inputData] : input Data또는 alert state가 변결될때만 실행해주세요. 그렇지 않으면 실행하지 말아주세요.
+    */
 
     let { id } = useParams(); // { 사용자가 입력한 URL 파라미터들 }, 스트럭처링 문법으로 처리
     let history = useHistory(); // 모든 방문기록이 담긴 object
@@ -39,9 +41,16 @@ function Detail ( props ) {
         <박스>
           <제목 className="red" >Detail</제목>
         </박스>
-        <div className={`my-alert2`}>
-          <p>재고가 얼마 남지 않았습니다.</p>
-        </div>
+        { inputData }
+        <input type="text" onChange={ (e)=> { inputData변경(e.target.value) } }/>
+        { // UI는 습관적으로 스위치 on/off 방식으로 관리하라~ (state로)
+          alert === true 
+          ? <div className={`my-alert2`}>
+              <p>재고가 얼마 남지 않았습니다.</p>
+            </div> 
+          : null
+        }
+        
         <div className="row">
           <div className="col-md-6">
             <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id+1}.jpg`} width="100%" />
